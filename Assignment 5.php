0c479f0906beddf77ecb90e3b6b1a3b032534f6c
Rushikeshkeskar <81505552+Rushikeshkeskar@users.noreply.github.com>
2021-05-31 08:08:06 +0530

    Add files via upload

---
+++ Assignment 5.php
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<bookstore>
+     <books category="Fantasy">
+	 <book_no>1</book_no>
+	 <book_name>The Lord of The Rings</book_name>
+	 <price>600</price>
+	 <year>1937</year>
+   </books>
+   <books category="Mystery">
+	 <book_no>2</book_no>
+	 <book_name>The Silent Patient</book_name>
+	 <price>150</price>
+	 <year>2019</year>
+	 </books>
+	<books category="Dystopian">
+	 <book_no>3</book_no>
+	 <book_name>Brave New World</book_name>
+	 <price>250</price>
+	 <year>1932</year>
+	 </books>
+</bookstore>
+ 
